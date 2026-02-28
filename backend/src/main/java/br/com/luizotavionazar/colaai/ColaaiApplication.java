9d6be4dd8711a1f71eda218df54e682eb7345592
Luiz Otavio <luizzotavionazar@outlook.com>
2026-02-28 20:55:04 -0300

    feat: esqueleto inicial

---
+++ backend/src/main/java/br/com/luizotavionazar/colaai/ColaaiApplication.java
@@ -0,0 +1,13 @@
+package br.com.luizotavionazar.colaai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColaaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ColaaiApplication.class, args);
+	}
+
+}
